db1a66bf4c679e3ca071c37947efb15bc209be13
Mehdi Hamime <mhamime@cloud.deloitte.fr>
2021-02-19 12:21:09 +0100

    update about & readme

--- README.md
+++ README.md
@@ -1,30 +1,39 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+COVID-19 Vaccinations Tracker, helps you browse data on COVID-19 (coronavirus) vaccinations.
+============================================================================================
 
-## Getting Started
+Casablanca, Morocco — February 22nd 2021 — Masked Developer today released COVID-19 Vaccinations Tracker, a web app that provide information about COVID-19 vaccinations.
 
-First, run the development server:
+You can browse worldwide, country-by-country or the latest statistics about vaccinations.
 
-```bash
-npm run dev
-# or
-yarn dev
-```
+COVID-19 Vaccinations Tracker Features
+--------------------------------------
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+*   Worldwide statistics
+    
+*   Vaccinations used in each country
+    
+*   Country-by-country data
+    
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+Availability
+------------
 
-## Learn More
+Currently COVID-19 Vaccinations Tracker is only available on the web.
 
-To learn more about Next.js, take a look at the following resources:
+* * *
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+### Additional Information
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+Source data from [Our World in Data](https://ourworldindata.org/) Built with [Next.js](https://nextjs.org/) & deployed on [Vercel](https://vercel.com/) SVG icons from [Heroicons](https://heroicons.com/) CSS blocks from [Tailblocks](https://github.com/mertJF/tailblocks)
 
-## Deploy on Vercel
+### Contact
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/import?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+Mehdi HAMIME (Masked Developer)
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+Casablanca,Morocco
+
+Email: mehdihamime@outlook.com
+
+Twitter: [@MehdiHAMIME](https://twitter.com/mehdihamime)
+
+Website: [www.maskeddeveloper.com](https://www.maskeddeveloper.com/)
--- pages/about.js
+++ pages/about.js
@@ -24,7 +24,7 @@ export default function Vaccines({ data }) {
                 </div>
                 <div class="flex flex-col items-center text-center justify-center">
                   <h2 class="font-medium title-font mt-4 text-gray-900 text-lg">
-                    Mehdi HAMIME
+                    Mehdi HAMIME (Masked Developer) 
                   </h2>
                   <div class="w-12 h-1 bg-yellow-500 rounded mt-2 mb-4"></div>
                   <p class="text-base">
@@ -36,30 +36,35 @@ export default function Vaccines({ data }) {
               </div>
               <div class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
                 <p class="leading-relaxed text-lg mb-4">
-                  Meggings portland fingerstache lyft, post-ironic fixie man bun
-                  banh mi umami everyday carry hexagon locavore direct trade art
-                  party. Locavore small batch listicle gastropub farm-to-table
-                  lumbersexual salvia messenger bag. Coloring book flannel
-                  truffaut craft beer drinking vinegar sartorial, disrupt
-                  fashion axe normcore meh butcher. Portland 90's scenester
-                  vexillologist forage post-ironic asymmetrical, chartreuse
-                  disrupt butcher paleo intelligentsia pabst before they sold
-                  out four loko. 3 wolf moon brooklyn.
+
+                <h1 id="covid-19-vaccinations-tracker-helps-you-browse-data-on-covid-19-coronavirus-vaccinations-">COVID-19 Vaccinations Tracker, helps you browse data on COVID-19 (coronavirus) vaccinations.</h1>
+<p>Casablanca, Morocco — February 22nd 2021 — Masked Developer today released COVID-19 Vaccinations Tracker, a web app that provide information about COVID-19 vaccinations. </p>
+<p>You can browse worldwide, country-by-country or the latest statistics about vaccinations. </p>
+<h2 id="covid-19-vaccinations-tracker-features">COVID-19 Vaccinations Tracker Features</h2>
+<ul>
+<li><p>Worldwide statistics </p>
+</li>
+<li><p>Vaccinations used in each country</p>
+</li>
+<li><p>Country-by-country data</p>
+</li>
+</ul>
+<h2 id="availability">Availability</h2>
+<p>Currently COVID-19 Vaccinations Tracker is only available on the web.</p>
+<hr>
+<h3 id="additional-information">Additional Information</h3>
+<p>Source data from <a href="https://ourworldindata.org/">Our World in Data</a>
+Built with <a href="https://nextjs.org/">Next.js</a> &amp; deployed on <a href="https://vercel.com/">Vercel</a> 
+SVG icons from <a href="https://heroicons.com/">Heroicons</a>
+CSS blocks from <a href="https://github.com/mertJF/tailblocks">Tailblocks</a></p>
+<h3 id="contact">Contact</h3>
+<p>Mehdi HAMIME (Masked Developer)</p>
+<p>Casablanca,Morocco</p>
+<p>Email: mehdihamime@outlook.com</p>
+<p>Twitter: <a href="https://twitter.com/mehdihamime">@MehdiHAMIME</a></p>
+<p>Website: <a href="https://www.maskeddeveloper.com/">www.maskeddeveloper.com</a></p>
                 </p>
-                <a class="text-yellow-500 inline-flex items-center">
-                  Learn More
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
+
               </div>
             </div>
           </div>
